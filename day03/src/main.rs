44e691b2fb6cf44a79f17a49b89d392c3f0afe90
Bastian Gruber <gruberbastian@me.com>
2019-02-18 13:50:05 +0100

    Day 03 Part 01 finished

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,12 +1,12 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader, Error, ErrorKind, Read};
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 fn main() -> Result<(), Error> {
     let vec = read(File::open("./src/input.txt")?)?;
-    let first: HashMap<u32, u32> = HashMap::new();
-    let more: HashMap<u32, u32> = HashMap::new();
-
+    let mut first: HashMap<String, String> = HashMap::new();
+    let mut more: HashMap<String, String> = HashMap::new();
+    
     for entry in vec.iter() {
         let at_sign = entry.find('@');
         let colon = entry.find(':');
@@ -36,13 +36,22 @@ fn main() -> Result<(), Error> {
         
         for wide in 0..fabric_wide {
             for tall in 0..fabric_tall {
-                
-
+                let x = left + wide;
+                let y = top + tall;
+                let coord = format!("{},{}", x, y);
+                let mark = String::from("X");
+                if first.contains_key(&coord) {
+                    if !more.contains_key(&coord) {
+                        more.insert(coord, mark);
+                    }
+                } else {
+                    first.insert(coord, mark);
+                }
             }
         }
-
-        println!("{:?}, {:?}", left, top);
     }
+    
+    println!("{:?}", more.len());
     Ok(())
 }
 
